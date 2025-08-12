0e00f6f69bd0e58e1c2953e7ead1e9de31523995
kalinsky-dev <kalin0169@gmail.com>
2025-08-12 12:21:52 +0300

    Refactor the code
    Improve code structure for maintainability and do some UI ehnancments

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,31 @@
 'use client';
 
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect, useCallback, MouseEvent } from 'react';
+import {
+  BOARD_WIDTH,
+  BOARD_HEIGHT,
+  GRID_SIZE,
+  NUMBER_OF_ROWS,
+  NUMBER_OF_COLUMNS,
+  DIRECTIONS,
+  COLORS,
+  createEmptyBoard,
+  Board,
+} from './utils/utils';
 
-type Board = number[][];
-
-const BOARD_WIDTH = 800;
-const BOARD_HEIGHT = 800;
-const GRID_SIZE = 20;
-const NUMBER_OF_ROWS = BOARD_HEIGHT / GRID_SIZE;
-const NUMBER_OF_COLUMNS = BOARD_WIDTH / GRID_SIZE;
-
-const COLORS = ['black', 'white'] as const;
-
-export const DIRECTIONS = [
-  [0, 1], // Right
-  [1, 1], // Down-Right
-  [1, 0], // Down
-  [1, -1], // Down-Left
-  [0, -1], // Left
-  [-1, -1], // Up-Left
-  [-1, 0], // Up
-  [-1, 1], // Up-Right
-];
-
-function createEmptyBoard(): Board {
-  return Array.from({ length: NUMBER_OF_ROWS }, () =>
-    Array.from({ length: NUMBER_OF_COLUMNS }, () => 0)
-  );
-}
-
-export default function Home() {
+export default function ConwayGameOfLife() {
   const emptyBoard = createEmptyBoard();
+
   const [boardState, setBoardState] = useState<Board>(emptyBoard);
   const [prevBoardState, setPrevBoardState] = useState<Board>(emptyBoard);
   const [isPlaying, setIsPlaying] = useState(false);
   const [generation, setGeneration] = useState(0);
   const [population, setPopulation] = useState(0);
   const [hasPreviousState, setHasPreviousState] = useState(false);
+
   const canvasRef = useRef<null | HTMLCanvasElement>(null);
   const playingRef = useRef(isPlaying);
   playingRef.current = isPlaying;
-
-  // Calculate population whenever board state changes
-  useEffect(() => {
-    let aliveCount = 0;
-    for (const row of boardState) {
-      for (const cell of row) {
-        if (cell === 1) aliveCount++;
-      }
-    }
-    setPopulation(aliveCount);
-  }, [boardState]);
 
   // console.log('Initial board state:', boardState);
 
@@ -85,6 +60,40 @@ export default function Home() {
       }
     }
   }, [boardState, canvasRef]);
+
+  // Calculate population whenever board state changes
+  useEffect(() => {
+    let aliveCount = 0;
+    for (const row of boardState) {
+      for (const cell of row) {
+        if (cell === 1) aliveCount++;
+      }
+    }
+    setPopulation(aliveCount);
+  }, [boardState]);
+
+  const handleCanvasClick = (e: MouseEvent<HTMLCanvasElement>) => {
+    if (playingRef.current) return; // Prevent editing while playing
+
+    const x = Math.floor(e.nativeEvent.offsetX / GRID_SIZE);
+    const y = Math.floor(e.nativeEvent.offsetY / GRID_SIZE);
+
+    const updatedBoardState = [...boardState];
+    if (updatedBoardState[x][y] === 0) {
+      updatedBoardState[x][y] = 1;
+    } else {
+      updatedBoardState[x][y] = 0;
+    }
+    setBoardState(updatedBoardState);
+  };
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+    if (!isPlaying) {
+      playingRef.current = true;
+      calculateNextGeneration();
+    }
+  };
 
   const calculateNextBoardState = (prevState: Board): Board => {
     const newBoardState = prevState.map((row) => [...row]);
@@ -121,6 +130,16 @@ export default function Home() {
     return newBoardState;
   };
 
+  const calculateNextGenerationOnce = () => {
+    setBoardState((prevState) => {
+      const prevBoard = prevState.map((row) => [...row]);
+      setPrevBoardState(prevBoard);
+      setHasPreviousState(true);
+      return calculateNextBoardState(prevState);
+    });
+    setGeneration((prev) => prev + 1);
+  };
+
   const calculateNextGeneration = useCallback(() => {
     if (!playingRef.current) {
       return;
@@ -135,29 +154,19 @@ export default function Home() {
     setTimeout(calculateNextGeneration, 100);
   }, [setBoardState, setPrevBoardState]);
 
-  const calculateNextGenerationOnce = useCallback(() => {
-    setBoardState((prevState) => {
-      const prevBoard = prevState.map((row) => [...row]);
-      setPrevBoardState(prevBoard);
-      setHasPreviousState(true);
-      return calculateNextBoardState(prevState);
-    });
-    setGeneration((prev) => prev + 1);
-  }, [setBoardState, setPrevBoardState]);
-
-  function calculatePreviousGeneration() {
+  const calculatePreviousGeneration = () => {
     if (!hasPreviousState) return;
     setBoardState(prevBoardState);
     setGeneration((prev) => Math.max(0, prev - 1));
     setHasPreviousState(false);
-  }
+  };
 
-  function resetBoard() {
+  const resetBoard = () => {
     setBoardState(createEmptyBoard());
     setPrevBoardState(createEmptyBoard());
     setGeneration(0);
     setHasPreviousState(false);
-  }
+  };
 
   return (
     <section className='flex flex-col items-center'>
@@ -179,7 +188,7 @@ export default function Home() {
           className={`px-4 py-2 rounded transition-colors ${
             !hasPreviousState
               ? 'bg-red-500 text-white cursor-not-allowed opacity-75'
-              : 'bg-gray-500 text-white hover:cursor-pointer hover:bg-gray-600'
+              : 'bg-gray-500 text-white hover:cursor-pointer hover:bg-gray-700'
           }`}
           onClick={calculatePreviousGeneration}
           disabled={!hasPreviousState}
@@ -187,44 +196,27 @@ export default function Home() {
           Previous
         </button>
         <button
-          className='bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer'
-          onClick={() => calculateNextGenerationOnce()}
+          className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 hover:cursor-pointer'
+          onClick={calculateNextGenerationOnce}
         >
           Next
         </button>
         <button
-          className='bg-red-500 text-white px-4 py-2 rounded hover:cursor-pointer'
+          className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 hover:cursor-pointer'
           onClick={resetBoard}
         >
           Reset
         </button>
         <button
-          className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 hover:cursor-pointer'
-          onClick={() => {
-            setIsPlaying(!isPlaying);
-            if (!isPlaying) {
-              playingRef.current = true;
-              calculateNextGeneration();
-            }
-          }}
+          className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 hover:cursor-pointer'
+          onClick={handlePlayPause}
         >
           {isPlaying ? 'Pause' : 'Play'}
         </button>
       </section>
 
       <canvas
-        onClick={(e) => {
-          const x = Math.floor(e.nativeEvent.offsetX / GRID_SIZE);
-          const y = Math.floor(e.nativeEvent.offsetY / GRID_SIZE);
-
-          const updatedBoardState = [...boardState];
-          if (updatedBoardState[x][y] === 0) {
-            updatedBoardState[x][y] = 1;
-          } else {
-            updatedBoardState[x][y] = 0;
-          }
-          setBoardState(updatedBoardState);
-        }}
+        onClick={handleCanvasClick}
         className='border border-green-500'
         ref={canvasRef}
         width={BOARD_WIDTH}
